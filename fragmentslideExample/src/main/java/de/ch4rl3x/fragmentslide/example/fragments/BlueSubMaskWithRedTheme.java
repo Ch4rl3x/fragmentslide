7b39d96b8f54b8424bd17d06c12ced6258218ab0
Alexander Karkossa <alexander.karkossa@googlemail.com>
2019-09-25 19:37:52 +0200

    Removing Example Project

--- fragmentslideExample/src/main/java/de/ch4rl3x/fragmentslide/example/fragments/BlueSubMaskWithRedTheme.java
+++
@@ -1,9 +0,0 @@
-package de.ch4rl3x.fragmentslide.example.fragments;
-
-import de.ch4rl3x.fragmentslide.example.abstracts.BaseFragment;
-
-public class BlueSubMaskWithRedTheme extends BaseFragment {
-
-}
-
-
